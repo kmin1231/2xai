7cca22c73719fc65e1b143c27169e7c01142c381
kmin1231 <kmin91143@gmail.com>
2025-03-23 02:50:05 +0900

    ✨ feat(app): integrate routes for each module

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@
 
 const express = require('express');
 const app = express();
+
 const authRoutes = require('./routes/auth.routes');
-const { swaggerUi, swaggerSpec } = require('./swagger');
+const textRoutes = require('./routes/text.routes');
+const teacherRoutes = require('./routes/teacher.routes');
+const studentRoutes = require('./routes/student.routes');
 
 // Swagger UI
+const { swaggerUi, swaggerSpec } = require('./swagger');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/text', textRoutes);
+app.use('/api/teacher', teacherRoutes);
+app.use('/api/student', studentRoutes);
 
 app.get('/', (req, res) => {
     res.send('[app.js] Server is running!');

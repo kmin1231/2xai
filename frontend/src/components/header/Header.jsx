29ab85097b4aa126aa20ab65894ec336bdf0434d
kmin1231 <kmin91143@gmail.com>
2025-04-21 00:18:17 +0900

    🎨 style: #1 update Header component

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,6 +1,9 @@
 // src/components/header/Header.jsx
 
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import './Header.css';
 
 const Header = () => {
@@ -8,6 +11,8 @@ const Header = () => {
     school: 'ㅇㅇ중학교',
     name: 'ㅇㅇㅇ 학생',
   });
+
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
   // should be implemented to display user information
   const displayUserInfo = (school, name) => {
@@ -18,10 +23,17 @@ const Header = () => {
   return (
     <header className="header">
       <div className="headerInfo">
-        {userInfo.school || "학교명"} &nbsp; | &nbsp; {userInfo.name || "이름"}
+
+    {/* depends on login status */}
+      {isLoggedIn ? (
+          `${userInfo.school || "학교명"} | ${userInfo.name || "이름"}`
+        ) : (
+          "프로그램명"
+        )}
+
       </div>
       <div className="nav">
-        <a href="#home">Home</a>
+      <Link to="/"><svg xmlns="http://www.w3.org/2000/svg" height="42px" viewBox="0 -960 960 960" width="42px" fill="#1f1f1f"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg></Link>
       </div>
     </header>
   );

344e550ef7c690ba38f15ed268e5e2ff4732355e
kmin1231 <kmin91143@gmail.com>
2025-04-21 00:21:05 +0900

    🎨 style: update global styles in index.css and App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import '@fontsource/ibm-plex-sans-kr/400.css';
+import '@fontsource/ibm-plex-sans-kr/700.css';
 
 import SplashPage from './pages/splash/Splash';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SplashPage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );

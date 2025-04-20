60704522f4f04b1cd6d8aecb5c7110fc74b18b05
kmin1231 <kmin91143@gmail.com>
2025-04-20 22:30:33 +0900

    ✨ feat(FE): #19 implement splash page

---
+++ frontend/src/pages/splash/Splash.jsx
@@ -0,0 +1,13 @@
+// src/pages/splash/Splash.jsx
+
+import React from 'react';
+import Splash from '@/components/splash/Splash';
+import './splash.css';
+
+const SplashPage = () => {
+    return (
+        <Splash />
+    );
+};
+
+export default SplashPage;

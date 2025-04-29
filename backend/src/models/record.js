172d8d2ee0fccb0b4d617d3c02a84079e50f4dd9
kmin1231 <kmin91143@gmail.com>
2025-04-30 08:33:54 +0900

    ✨ feat(text): #23 update database schema (Text, Record)

--- backend/src/models/record.js
+++ backend/src/models/record.js
@@ -1,14 +1,14 @@
 // models/record.js
 
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const recordSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   textId: { type: Schema.Types.ObjectId, ref: 'Text', required: true },
-  isCorrect: { type: Boolean, required: true },
+  correctness: { type: [Boolean], required: true },
 }, { timestamps: true });
 
-const Record = mongoose.model('Record', recordSchema);
+const Record = mongoose.model('Record', recordSchema, "records");
 
 module.exports = Record;

57baa77bf22bf445231ee201e6f873a4e0bf35c6
kmin1231 <kmin91143@gmail.com>
2025-04-29 15:56:11 +0900

    ✨ feat(DB): #14 update 'User' schema (MongoDB subdocument)

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -8,8 +8,22 @@ const userSchema = new mongoose.Schema({
   name: { type: String },
   class: { type: String },
   school: { type: String },
+  class_id: { type: Schema.Types.ObjectId, ref: 'Class' },
   role: { type: String, required: true, enum: ['student', 'teacher', 'admin'] },
   level: { type: String, enum: ['high', 'middle', 'low'], default: 'low' },
+
+  student_info: {
+    inferred_level: { type: String, enum: ['high', 'middle', 'low'] },  // 알고리즘에 의한 level
+    assigned_level: { type: String, enum: ['high', 'middle', 'low'] },  // 교사 지정 level
+  },
+
+  teacher_info: {
+    class_ids: [{ type: Schema.Types.ObjectId, ref: 'Class' }]
+  }
+
+}, {
+  timestamps: true,
+  versionKey: false
 });
 
 const User = mongoose.model('User', userSchema);

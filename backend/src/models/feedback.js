c53efd5fb575cc48b7813f8cf896753cbe428f08
kmin1231 <kmin91143@gmail.com>
2025-04-30 04:13:12 +0900

    ✨ feat(text): #23 update 'Feedback' schema

--- backend/src/models/feedback.js
+++ backend/src/models/feedback.js
@@ -1,32 +1,19 @@
 // models/feedback.js
 
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const User = require('./user');
-const Text = require('./text');
+const { Schema } = mongoose;
 
 const feedbackSchema = new mongoose.Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  textId: { type: Schema.Types.ObjectId, ref: 'Text', required: true },
-  feedback: {
-    type: String,
-    enum: ['easy', 'hard', 'boring'],
-    required: true,
-  },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true, },
+  keyword: { type: String, required: true, },
+  level: { type: String, required: true, },
+  feedbacks: [{
+    title: { type: String, required: true, },
+    passage: { type: String, required: true, },
+    feedback: { type: String, required: true, enum: ['good', 'too_easy', 'too_hard', 'not_interesting'], },
+  }],
 });
 
-
-feedbackSchema.pre('save', async function (next) {
-  const user = await User.findById(this.userId);
-  const text = await Text.findById(this.textId);
-
-  this.userName = user ? user.name : null;
-  this.textPassage = text ? text.passage : null;
-
-  next();
-});
-
-const Feedback = mongoose.model('Feedback', feedbackSchema);
+const Feedback = mongoose.model('Feedback', feedbackSchema, "feedbacks");
 
 module.exports = Feedback;

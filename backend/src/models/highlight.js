531e8305b8fb0a93c0ff2a9ac6ab7b9ac418d3da
kmin1231 <kmin91143@gmail.com>
2025-04-30 05:51:24 +0900

    ✨ feat(text): #23 update 'Highlight' schema

--- backend/src/models/highlight.js
+++ backend/src/models/highlight.js
@@ -1,19 +1,13 @@
 // models/highlight.js
 
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const User = require('./user');
-const Text = require('./text');
+const { Schema } = mongoose;
 
 const highlightSchema = new mongoose.Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    textId: { type: Schema.Types.ObjectId, ref: 'Text', required: true },
-    // start: { type: Number, required: true },
-    // end: { type: Number, required: true },
     text: { type: String, required: true },
 });
 
-const Highlight = mongoose.model('Highlight', highlightSchema);
+const Highlight = mongoose.model('Highlight', highlightSchema, "highlights");
 
 module.exports = Highlight;

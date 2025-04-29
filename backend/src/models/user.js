e50091bc0c646d04ba169041756ce2a4537658e0
kmin1231 <kmin91143@gmail.com>
2025-04-29 17:22:03 +0900

    ✨ feat(auth): #28 update 'User' schema and add 'Class' schema

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,20 +1,18 @@
 // models/user.js
 
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true  },
   password: { type: String, required: true },
   name: { type: String },
-  class: { type: String },
-  school: { type: String },
   class_id: { type: Schema.Types.ObjectId, ref: 'Class' },
   role: { type: String, required: true, enum: ['student', 'teacher', 'admin'] },
-  level: { type: String, enum: ['high', 'middle', 'low'], default: 'low' },
 
   student_info: {
-    inferred_level: { type: String, enum: ['high', 'middle', 'low'] },  // 알고리즘에 의한 level
-    assigned_level: { type: String, enum: ['high', 'middle', 'low'] },  // 교사 지정 level
+    inferred_level: { type: String, enum: ['high', 'middle', 'low'], default: 'low' },  // 알고리즘에 의한 level
+    assigned_level: { type: String, enum: ['high', 'middle', 'low'], default: 'low' },  // 교사 지정 level
   },
 
   teacher_info: {
@@ -26,6 +24,6 @@ const userSchema = new mongoose.Schema({
   versionKey: false
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema, 'users');
 
 module.exports = User;

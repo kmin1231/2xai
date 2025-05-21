38eecd3272964a8febf2113ec0f370f892bbe719
kmin1231 <kmin91143@gmail.com>
2025-05-21 09:40:39 +0900

    ✨ feat(FE): enable toast notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import '@fontsource/ibm-plex-sans-kr/400.css';
 import '@fontsource/ibm-plex-sans-kr/700.css';
@@ -15,14 +18,18 @@ import CustomLevelScore from './pages/student/mode-custom/Score';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<SplashPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/student" element={<StudentMain />} />
-        <Route path="/student/mode/custom" element={<CustomLevelKeyword />} />
-        <Route path="/student/mode/custom/result" element={<CustomLevelResult />} />
-        <Route path="/student/mode/custom/score" element={<CustomLevelScore />} />
-      </Routes>
+      <>
+        <Routes>
+          <Route path="/" element={<SplashPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/student" element={<StudentMain />} />
+          <Route path="/student/mode/custom" element={<CustomLevelKeyword />} />
+          <Route path="/student/mode/custom/result" element={<CustomLevelResult />} />
+          <Route path="/student/mode/custom/score" element={<CustomLevelScore />} />
+        </Routes>
+
+        <ToastContainer position="bottom-center" autoClose={3000} />
+      </>
     </Router>
   );
 }

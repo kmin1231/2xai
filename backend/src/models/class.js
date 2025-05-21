5846e56967907d06d1451a91ec1084f00bf54d41
kmin1231 <kmin91143@gmail.com>
2025-05-21 19:51:37 +0900

    ✨ feat(teacher): #35 update 'Class' schema - class_keyword

--- backend/src/models/class.js
+++ backend/src/models/class.js
@@ -6,9 +6,10 @@ const { Schema } = mongoose;
 const classSchema = new mongoose.Schema({
     class_name: { type: String },
     school_name: { type: String },
-    teacher: { type: Schema.Types.ObjectId, ref: 'User' },          // 담당 교사 (User.role = teacher)
-    student_ids: [{ type: Schema.Types.ObjectId, ref: 'User' }],    // 소속 학생들의 목록 (User.role = student)
-    class_level: { type: String, enum: ['high', 'middle', 'low'], default: 'low' }  // 담당 교사가 지정한 level
+    teacher: { type: Schema.Types.ObjectId, ref: 'User' },                          // 담당 교사 (User.role = teacher)
+    student_ids: [{ type: Schema.Types.ObjectId, ref: 'User' }],                    // 소속 학생들의 목록 (User.role = student)
+    class_level: { type: String, enum: ['high', 'middle', 'low'], default: 'low' }, // 담당 교사가 지정한 level
+    class_keyword: { type: String }                                                 // 담당 교사가 지정한 keyword
 }, {
     timestamps: true,
     versionKey: false

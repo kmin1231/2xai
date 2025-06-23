4d7d97f1a3da5d00646fb3963bcc34442ce87b10
kmin1231 <kmin91143@gmail.com>
2025-06-23 22:14:33 +0900

    ✨ feat: add HTTPS support using SSL certificate

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,26 @@
 
 require('dotenv').config();
 
+const fs = require('fs');
+const https = require('https');
+
 const app = require('./app');
 const connectDB = require('./db/connect');
-const User = require('./models/user');
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 443;
 const SERVER_URL = process.env.SERVER_URL || "http://localhost";
 
+const SSL_KEY_PATH = process.env.SSL_KEY_PATH;
+const SSL_CERT_PATH = process.env.SSL_CERT_PATH;
 
 connectDB().then(() => {
-    // createTestUsers();
-    app.listen(PORT, () => {
-      console.log(`Server is running on ${SERVER_URL}:${PORT}`);
-      console.log(`Swagger UI is available on ${SERVER_URL}:${PORT}/api-docs`);
-    });
+  const sslOptions = {
+    key: fs.readFileSync(SSL_KEY_PATH),
+    cert: fs.readFileSync(SSL_CERT_PATH),
+  };
+  
+  https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`Server is running on ${SERVER_URL}:${PORT}`);
+    console.log(`Swagger UI is available on ${SERVER_URL}:${PORT}/api-docs`);
+  });
 });

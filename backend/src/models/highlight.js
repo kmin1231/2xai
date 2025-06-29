d0e2c341d013fb3a9ef25af0969c40315ff9dd77
kmin1231 <kmin91143@gmail.com>
2025-06-29 23:59:38 +0900

    ✨ feat(text): #65 update 'Highlight' schema

--- backend/src/models/highlight.js
+++ backend/src/models/highlight.js
@@ -6,6 +6,18 @@ const { Schema } = mongoose;
 const highlightSchema = new mongoose.Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     text: { type: String, required: true },
+
+    label: {
+      type: String,
+      enum: ['important', 'confusing', 'mainidea', 'etc'],
+      default: 'etc',
+      required: true,
+    },
+
+    imageUrl: {
+      type: String,
+      required: false,
+    },
 }, {
   timestamps: { createdAt: true, updatedAt: false },
   versionKey: false,

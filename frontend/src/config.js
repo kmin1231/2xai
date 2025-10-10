0f097e68e4e930f5a55080b17fafee22e5392d4d
Eden Min Kim <kmin91143@gmail.com>
2025-10-10 16:02:25 +0900

    🧹 chore: revert temporary HTTP API URL to HTTPS

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const isProduction = process.env.NODE_ENV === 'production';
 
 const BASE_URL = isProduction
-  ? 'http://www.twoxai.online/api' // deployment server URL
+  ? 'https://www.twoxai.online/api' // deployment server URL
   : 'http://localhost:3200/api';    // development server URL
 
 
